b44a91aa9b76eba3605e3293387cf0d85acdb3ca
Hannah Walden <hannahfaithwalden@gmail.com>
2023-12-08 01:41:15 -0500

    Add Day 8

---
+++ Day 8/AOC23 08p1.py
@@ -0,0 +1,37 @@
+file_path = "Day 8/input.txt"
+
+with open(file_path, 'r') as file:
+    lines_array = [line.strip() for line in file.readlines()]
+
+directions = lines_array[0].split(", ")
+lines_array.pop(0)
+lines_array.pop(0)
+node_dictionary = {}
+
+def create_node_dict(node_map):
+    for node in node_map:
+        temp_array = node.split(" ")
+        temp_array.pop(1)
+        temp_array[1] = temp_array[1][1:4]
+        temp_array[2] = temp_array[2][:3]
+
+        node_dictionary[temp_array[0]] = [temp_array[1], temp_array[2]]
+
+create_node_dict(lines_array)
+
+current_node = "AAA"
+ending_node = "ZZZ"
+steps = 0
+flag = True
+
+while flag:
+    for each in directions[0]:
+        steps += 1
+        if each == "R":
+            current_node = node_dictionary[current_node][1]
+        else:
+            current_node = node_dictionary[current_node][0]
+        if current_node == ending_node:
+            flag = False
+            print(f"steps: {steps}")
+            break
